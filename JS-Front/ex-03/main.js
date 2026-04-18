cd8566858298443b9b0af1fd990405105d688bcb
_Drwax_P <161478981+DrwaxP@users.noreply.github.com>
2026-04-18 09:27:53 -0300

    Add files via upload

---
+++ JS-Front/ex-03/main.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById('seach')
+
+    btn.addEventListener('click', () => {
+        let nasc = document.getElementsByClassName('input.nasc');
+        let masc = document.getElementsByClassName('input.masculino');
+        let fem = document.getElementsByClassName('input.feminino');
+    })
