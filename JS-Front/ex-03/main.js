5b1cfd118a375dbb72d8e54343c138052f64443e
_Drwax_P <161478981+DrwaxP@users.noreply.github.com>
2026-04-18 15:32:11 -0300

    Add files via upload

--- JS-Front/ex-03/main.js
+++ JS-Front/ex-03/main.js
@@ -1,7 +1,33 @@
-let btn = document.getElementById('seach')
+let data = new Date()
+let ano = data.getFullYear();
+let btn = document.getElementById('search');
 
-    btn.addEventListener('click', () => {
-        let nasc = document.getElementsByClassName('input.nasc');
-        let masc = document.getElementsByClassName('input.masculino');
-        let fem = document.getElementsByClassName('input.feminino');
-    })
+btn.addEventListener('click', () => {
+    let nasc = document.querySelector('input.nasc');
+    let mascSelected = document.querySelector('input.masculino').checked;
+    let femSelected = document.querySelector('input.feminino').checked;
+    let total = Number(ano - nasc.value);
+    let texto = document.getElementsByTagName('p')[0];
+
+    if(nasc.value == 0 || Number(nasc.value) > ano) {
+        alert('[Error] Digite um valor válido ou coloque os dados corretos.');
+        return
+    }
+
+
+    let gen = '';
+    if(mascSelected === true) {
+       gen = 'Masculino'
+            
+    }
+    else if(femSelected === true) {
+        gen = 'Feminino'
+        
+    } else {
+        alert('Dados incorretos ou faltando. Reinicie e tente novamente.');
+        return
+    }
+    texto.innerHTML = `Você tem ${total} anos. Seu sexo e ${gen}. `
+    
+});
+ 
